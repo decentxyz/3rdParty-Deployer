aecebbceaa3cfac184e8a72aac749ac1ef8da8c5
metabaronespinosa <axnotliztac@gmail.com>
2023-02-15 20:50:13 -0600

    init r3vl form

---
+++ components/Form.tsx
@@ -0,0 +1,196 @@
+import { useEffect, useMemo, useState } from 'react'
+
+import { useAccount, useEnsName, useNetwork, useProvider, useSigner } from 'wagmi'
+import { useR3vlClient, useCreateRevenuePath, R3vlProvider, createClient, useBalances, useRevenuePathTiers, useUpdateRevenuePath } from '@r3vl/sdk'
+import { useRouter } from 'next/router'
+
+// const revPathAddress = "0x3920620177D55DA7849237bb932E5112005d4A04"
+
+const Form = ({ revPathAddress }: any) => {
+  const { chain } = useNetwork()
+  const { address } = useAccount()
+  const provider = useProvider()
+  const { data: signer } = useSigner()
+  const { data: ens } = useEnsName({ address })
+  const [pathName, setPathName] = useState("")
+  const [collabs, setCollabs] = useState<{ address?: string; share: number }[]>(revPathAddress ? [] : [{ address, share: 100 }])
+  const [error, setError] = useState("")
+  const collabsMemo = useMemo(() => {
+    return collabs
+  }, [collabs.reduce((prev, curr) => {
+    return prev + curr.share
+  }, collabs.length), collabs])
+
+  console.log("Q·!", collabsMemo)
+
+  useR3vlClient({
+    chainId: chain?.id as any,
+    provider,
+    signer: signer as any
+  })
+
+  useR3vlClient({
+    chainId: chain?.id as any,
+    provider,
+    signer: signer as any,
+    revPathAddress
+  })
+    
+  const { mutate, data: tx } = useCreateRevenuePath()
+  const { data } = useBalances(revPathAddress)
+  const { data: tiers, isFetched: tiersFetched } = useRevenuePathTiers(revPathAddress, { enabled: !!revPathAddress })
+  const update = useUpdateRevenuePath(revPathAddress)
+
+  useEffect(() => {
+    const tier = tiers?.[0]
+
+    if (!tiersFetched) return
+    if (!tier) return
+
+    const newCollabs = Object.keys(tier.proportions as any).reduce((prev: any, curr) => {
+      return [...prev, { address: curr, share: tier.proportions[curr] * 10000000000000 }]
+    }, [])
+
+    setCollabs(newCollabs as any)
+  }, [tiers, tiersFetched, setCollabs, address])
+
+  useEffect(() => {
+    if (tx) {
+      (tx as any).wait().then((r: any) => console.log(r.logs[0].address))
+    }
+  }, [tx])
+
+  const submitPath = () => {
+    const sum = collabs.reduce((prev, curr) => {
+      return prev + curr.share
+    }, 0)
+
+    if (sum > 100 || sum < 100) {
+      setError("Total share must be equal to 0")
+
+      return
+    }
+
+    mutate({
+      name: pathName,
+      walletList: [
+        collabs.map(collab => collab.address) as any
+      ],
+      distribution: [
+        collabs.map(collab => collab.share) as any
+      ],
+      mutabilityDisabled: false
+    })
+  }
+
+  const updatePath = () => {
+    const sum = collabs.reduce((prev, curr) => {
+      return prev + curr.share
+    }, 0)
+  
+    if (sum > 100 || sum < 100) {
+      setError("Total share must be equal to 0")
+  
+      return
+    }
+
+    update?.updateRevenueTiers.mutate({
+      walletList: [collabs.map(collab => collab.address) as any],
+      distribution: [collabs.map(collab => collab.share)],
+      tierNumbers: [0],
+    })
+  }
+
+  return <>
+    <div className='flex flex-col gap-4'>
+          <input onChange={(e) => setPathName(e.currentTarget.value)} className='border rounded-full w-3/4 px-3 py-2 text-sm' placeholder='Revenue Path Name' />
+
+          {collabsMemo.map((collab, i) => {
+            return <div key={i} className='flex gap-2'>
+              <div className='flex relative items-center w-1/4'>
+                <input
+                  onChange={(e) => {
+                    const newCollabs = collabs.map((_, index) => {
+                      if (index === i) return { address: collab.address, share: parseFloat(e.currentTarget.value || "0") }
+
+                      return _
+                    })
+
+                    setCollabs(newCollabs)
+                    setError("")
+                  }}
+                  className='border rounded-full w-full px-3 py-2 text-sm'
+                  value={collab.share}
+                  placeholder="%"
+                />
+                <p className="text-sm absolute right-3">%</p>
+              </div>
+              <input
+                onChange={(e) => {
+                  const newCollabs = collabs.map((_, index) => {
+                    if (index === i) return { address: e.currentTarget.value, share: collab.share }
+
+                    return _
+                  })
+
+                  setCollabs(newCollabs)
+                }}
+                placeholder='0x1234...56aB'
+                className='border rounded-full w-3/4 px-3 py-2 text-sm'
+                value={collab.address}
+              />
+              <button className='text-red-600 text-xs' onClick={() => {  
+                const newCollabs = collabs.filter((_, index) => index !== i)
+  
+                setCollabs([])
+
+                setCollabs([...newCollabs])
+              }}>
+                Delete
+              </button>
+            </div>
+          })}
+
+          <p className='text-red-600'>{error}</p>
+
+          <div className='flex gap-4 w-1/3 mt-4'>
+            <button
+              className='flex items-center justify-center gap-4 py-1 px-3 text-sm border rounded-full border-black max-h-[40px]'
+              onClick={() => setCollabs([ ...collabs, { address: '', share: 0 } ])}
+            >
+              Add
+            </button>
+
+            {tiersFetched && !tiers?.[0] && <button
+              className='cursor-pointer py-2 px-3 bg-black text-white rounded-full flex justify-center max-h-[40px]'
+              onClick={submitPath}
+            >
+              Create
+            </button>}
+            {tiersFetched && tiers?.[0] && <button
+              className='cursor-pointer py-2 px-3 bg-black text-white rounded-full flex justify-center max-h-[40px]'
+              onClick={() => {
+                updatePath()
+              }}
+            >
+              Update Path
+            </button>}
+          </div>
+          {data && <div className='flex gap-4 w-2/3 mt-4 align-center justify-start'>
+            <div>
+              <p className="font-medium">Balance</p>
+              <p className="font-header">{data?.withdrawable} ETH</p>
+            </div>
+
+            <button
+              className='cursor-pointer py-2 px-3 bg-black text-white rounded-full flex justify-center max-h-[40px]'
+              onClick={submitPath}
+            >
+              Withdraw
+            </button>
+          </div>}
+        </div>
+  </>
+}
+
+export default Form
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,17 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { R3vlProvider, createClient } from '@r3vl/sdk';
+import Form from '../components/Form';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  const r3vlClient = createClient()
+
   return (
-    <div className={`${styles.container} background`}>
+    <R3vlProvider client={r3vlClient}>
+    <div className={`${styles.container} bg-white`}>
       <Head>
         <title>Start off Decent</title>
         <meta
@@ -18,71 +25,21 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 mb-20">
         <ConnectButton />
           <Link href='https://github.com/decentxyz/Start-Decent' target='_blank'>
             <Image src='/images/github-mark-white.svg' height={22} width={22} alt='link to repository' />
           </Link>
         </div>
 
-        <h1 className={`${styles.title} font-medium`}>
-          Welcome to the Decent Protocol
-        </h1>
-
-        <div className={`${styles.description} flex items-center gap-2`}>
-          <p>Powered by</p>
-          <a className='pt-2' href="https://decentxyz.gitbook.io/decent-sdk-documentation/"><Image src='/images/icon.png' height={20} width={25} alt='decent icon'/></a>
-          <a href="https://rainbowkit.com" className='text-lg'> + 🌈</a>
-          <a href='href="https://nextjs.org/docs"'> + Next.js</a>
-        </div>
-
-        <div className={`${styles.grid} cursor-pointer`}>
-          <Link href='/deploy-contracts'>
-          <div className={styles.card}>
-            <h2 className='font-medium'>Test Decent Editions &rarr;</h2>
-            <p>Deploying sophisticated contracts has never been easier.</p>
-          </div>
-          </Link>
-
-          <a href="https://decentxyz.gitbook.io/decent-sdk-documentation/" className={styles.card}>
-            <h2 className='font-medium'>Decent Protocol Docs &rarr;</h2>
-            <p>Review smart contracts and architecture.</p>
-          </a>
-
-          <a href="https://rainbowkit.com" className={styles.card}>
-            <h2 className='font-medium'>RainbowKit Docs &rarr;</h2>
-            <p>Learn how to customize your wallet connection flow.</p>
-          </a>
-
-          <a href="https://wagmi.sh" className={styles.card}>
-            <h2 className='font-medium'>wagmi Docs &rarr;</h2>
-            <p>Learn how to interact with Ethereum.</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2 className='font-medium'>Next.js Docs &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://hq.decent.xyz" className={styles.card}>
-            <h2 className='font-medium'>Decent Creator Studio &rarr;</h2>
-            <p>
-              {`Explore Decent's creator studio for no-code deployments.`}
-            </p>
-          </a>
-        </div>
+        <Form revPathAddress={router.query.revPath}  />
       </main>
 
-      <footer className='py-8 border-t border-white text-white'>
-        <div>
-        <p className='flex justify-center pb-4 text-xl'>For the artists of every industry 🥂</p>
-        <a className='flex justify-center items-center text-xl' href="https://decent.xyz" target="_blank" rel="noopener noreferrer">
-         <span className='pr-4'>🏗️</span> 
-        <Image src='/images/decent.png' height={18} width={100} alt='Decent 💪' />
-        </a>
-        </div>
+      <footer className='py-8 border-t border-white'>
+        <div />
       </footer>
     </div>
+    </R3vlProvider>
   );
 };
 
